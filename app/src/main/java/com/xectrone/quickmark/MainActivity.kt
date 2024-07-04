1b471a3f25c86fdf12232589d9e5391e39f37c66
Bhushan Malekar <84770287+xectrone@users.noreply.github.com>
2024-07-04 20:27:31 +0530

    version 24
    Added Animations

--- app/src/main/java/com/xectrone/quickmark/MainActivity.kt
+++ app/src/main/java/com/xectrone/quickmark/MainActivity.kt
@@ -3,18 +3,20 @@ package com.xectrone.quickmark
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.animation.ExperimentalAnimationApi
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.xectrone.flashup.ui.theme.QuickMarkTheme
 import com.xectrone.quickmark.domain.navigation.HomeScreenNavGraph
 
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_QuickMark)
         setContent {
             QuickMarkTheme {
-                HomeScreenNavGraph(navController = rememberNavController())
+                HomeScreenNavGraph(navController = rememberAnimatedNavController())
             }
 
         }
--- app/src/main/java/com/xectrone/quickmark/domain/navigation/HomeScreenNavGraph.kt
+++ app/src/main/java/com/xectrone/quickmark/domain/navigation/HomeScreenNavGraph.kt
@@ -1,29 +1,41 @@
 package com.xectrone.quickmark.domain.navigation
 
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
 import com.xectrone.quickmark.domain.Util
 import com.xectrone.quickmark.ui.add_edit_note_screen.AddEditNoteScreen
 import com.xectrone.quickmark.ui.home_screen.HomeScreen
 import com.xectrone.quickmark.ui.settings_screen.SettingsScreen
 import com.xectrone.quickmark.ui.theme.Constants.FILE_URI
+import com.xectrone.quickmark.ui.theme.CustomAnimations
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun HomeScreenNavGraph(navController: NavHostController)
 {
-    NavHost(navController = navController, startDestination = Screen.Home.route )
+    AnimatedNavHost(navController = navController, startDestination = Screen.Home.route )
     {
         composable(route = Screen.Home.route) {
             HomeScreen(navController = navController)
         }
-        composable(route = Screen.Setting.route) {
+        composable(route = Screen.Setting.route,
+            enterTransition = { CustomAnimations.slideInHorizontally() },
+            exitTransition = { CustomAnimations.slideOutHorizontally() },
+            popEnterTransition = { CustomAnimations.slideInHorizontally() },
+            popExitTransition = { CustomAnimations.slideOutHorizontally() },
+        ) {
             SettingsScreen(navController = navController)
         }
         composable(route = Screen.AddEditNote.full,
+            enterTransition = { CustomAnimations.slideInVertically() },
+            exitTransition = { CustomAnimations.slideOutVertically() },
+            popEnterTransition = { CustomAnimations.slideInVertically() },
+            popExitTransition = { CustomAnimations.slideOutVertically() },
             arguments = listOf(
                 navArgument(FILE_URI) {
                     type = NavType.StringType
