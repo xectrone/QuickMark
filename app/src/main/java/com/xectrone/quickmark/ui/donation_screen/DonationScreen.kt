a0440c8a9711599c7813c56dfa675dc788fa8a7a
Bhushan Malekar <84770287+xectrone@users.noreply.github.com>
2025-06-29 16:36:26 +0530

    version 42
    added support for sharing URL from browser

--- app/src/main/java/com/xectrone/quickmark/ui/donation_screen/DonationScreen.kt
+++ app/src/main/java/com/xectrone/quickmark/ui/donation_screen/DonationScreen.kt
@@ -24,7 +24,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
